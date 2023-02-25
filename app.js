f53ee232102e1863a5ec80903ac520f8c6d4a967
Ashish <ashishsaini1003@gmail.com>
2023-02-25 23:33:37 +0530

    used parcel for budling and hosting

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const heading=React.createElement("h1",{id:"hello"},"Helllllo");
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+const heading1=React.createElement("h1",{id:"hello"},"Heading 1");
+
+const heading2=React.createElement("h1",{id:"hello"},"Heading 2");
+
+const div=React.createElement("div",{id:"container"},[heading1,heading2]);
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(div);
