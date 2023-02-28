d5aa65d7e825b0ab595235cea1f92bbd58f977d3
Ashish <ashishsaini1003@gmail.com>
2023-02-28 22:24:02 +0530

    assignment 3

---
+++ components/search.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const SearchBar =()=> {
+
+  return (
+    <form style={{marginLeft:"auto", marginRight:"10px"}}>
+      <input type="text" />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;
