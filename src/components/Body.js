438ed3977eeafe728507b912bbcb404c11fcbede
Ashish <ashishsaini1003@gmail.com>
2023-03-02 21:06:10 +0530

    Added search functanality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
-
-
+import SearchBar from "./search";
 const Body = () =>{
+
+    const [allRestaurant,setAllRestaurant]= useState(restaurantList);
+    
+    const [restaurant,setRestaurant]= useState(restaurantList);
+    
+    function restaurantFilter(text){
+        const data=allRestaurant.filter((restaurant)=>restaurant.data.name.toLowerCase().includes(text.toLowerCase()));
+        setRestaurant(data);
+    }
     return(
+        <>
+        <SearchBar restaurantFilter={restaurantFilter}/>
         <div id="Body" >
-           {restaurantList?.map((restaurant)=>{
+           {restaurant?.map((restaurant)=>{
                 return <RestaurantCard restaurant={restaurant.data}/>;
            })}
         </div>
+        </>
     )
 }
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
-const SearchBar =()=> {
+const SearchBar =({restaurantFilter})=> {
 
+  const [text,setText]=useState("");
   return (
-    <form style={{marginLeft:"auto", marginRight:"10px"}}>
-      <input type="text" />
-      <button type="submit">Search</button>
-    </form>
+    <div id="search">
+      <input type="text" className="search" placeholder="search" value={text} onChange={(e)=>setText(e.target.value)}/>
+      <button type="submit" onClick={()=>{restaurantFilter(text)}}>Search</button>
+    </div>
   );
 }
 
